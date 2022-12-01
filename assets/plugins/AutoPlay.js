49bf04c683ca49102fec5d03d5012508ee4feb3d
BrnMarq <brnmarq@gmail.com>
2022-11-30 21:32:41 -0400

    🎉 Begin promises class

--- assets/plugins/AutoPlay.js
+++ assets/plugins/AutoPlay.js
@@ -1,8 +1,7 @@
 function AutoPlay() {}
-
-AutoPlay.prototype.run = function (player) {
-	player.mute();
-	player.play();
+AutoPlay.prototype.run = function(player) {
+  player.mute();
+  player.play();
 };
 
 export default AutoPlay;
